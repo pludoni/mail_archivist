4b7a3a9d3cc4d5d3fe7d644d498fa7b5ab90a957
Wolfgang Wohanka <wolfgang.wohanka@pludoni.de>
2019-02-25 17:27:27 +0100

    fix(MailLog): ordne den Spalten die korrekte Bezichnung zu

--- app/models/mail_archivist/action_mailer_hook.rb
+++ app/models/mail_archivist/action_mailer_hook.rb
@@ -1,11 +1,11 @@
 module MailArchivist
   class ActionMailerHook
     def self.delivered_email(message)
-      MailArchivist::MailLog.create(to: (message.to||[]).join(','),
-                      cc: (message.to||[]).join(','),
-                      from: (message.to||[]).join(','),
-                      subject:  message.subject,
-                      body:  (message.text_part || message.html_part || message.body).to_s)
+      MailArchivist::MailLog.create(to: (message.to || []).join(','),
+                                    cc: (message.cc || []).join(','),
+                                    from: (message.from || []).join(','),
+                                    subject: message.subject,
+                                    body: (message.text_part || message.html_part || message.body).to_s)
     end
   end
 end
